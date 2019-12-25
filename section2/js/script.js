96666bb983a35dfd16fbc7040ae4aff8f82703a9
Tatsuro <contacts@tatsuro.dev>
2019-12-26 03:38:28 +0900

    簡易的演習 33

--- section2/js/script.js
+++ section2/js/script.js
@@ -1,7 +1,8 @@
-Vue.component('hello-component', {
-    template: '<p>Hello</p>',
-})
-
 var app = new Vue({
     el: '#app',
+    data: {
+        message: 'Hello World!',
+        url: 'https://jp.vuejs.org/',
+        toggle: true,
+    }
 })
