2253687099db40384e55c89f9ab9f19e254bc195
Tatsuro <contacts@tatsuro.dev>
2019-12-26 03:07:48 +0900

    v-model directiveで双方向data binding 31

--- section2/js/script.js
+++ section2/js/script.js
@@ -1,14 +1,6 @@
 var app = new Vue({
     el: '#app',
     data: {
-        now: '',
-    },
-    methods: {
-        onclick: function () {
-            // alert('onclick!')
-            // thisでdataへaccess
-            this.now = new Date().toLocaleString()
-        }
+        message: 'Hello Vue.js!',
     }
-
 })
