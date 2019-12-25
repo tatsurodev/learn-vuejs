0aed64377db381aef6b75535e85c2bbdfd576e94
Tatsuro <contacts@tatsuro.dev>
2019-12-26 02:02:57 +0900

    属性へのdata binding、v-bind directiveの使用 24

--- section2/js/script.js
+++ section2/js/script.js
@@ -1,14 +1,6 @@
 var app = new Vue({
-    // options
     el: '#app',
     data: {
         message: 'Hello Vue.js!',
-        count: 10,
-        user: {
-            lastName: 'Yamada',
-            firstName: 'Taro',
-            prefecture: 'Tokyo',
-        },
-        colors: ['Red', 'Green', 'Blue',]
     }
 })
