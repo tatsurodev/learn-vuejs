a01f521195642704cb8bb1dd8e467fe80ca30759
Tatsuro <contacts@tatsuro.dev>
2019-12-26 06:22:54 +0900

    inputのv-model bindingで取得したtodoをtodosにbuttonのevent handlingで追加 42

--- section3/js/script.js
+++ section3/js/script.js
@@ -2,10 +2,15 @@ var app = new Vue({
     el: '#app',
     data: {
         newItem: '',
+        todos: [],
     },
     methods: {
         addItem: function (event) {
-            alert()
+            // alert()
+            var todo = {
+                item: this.newItem,
+            }
+            this.todos.push(todo)
         }
     }
 })
