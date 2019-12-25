9e59aa43bb892dc9311c519144b38f2b5ffce49c
Tatsuro <contacts@tatsuro.dev>
2019-12-26 02:26:09 +0900

    v-for directiveでdata objectを繰り返し処理 29

--- section2/js/script.js
+++ section2/js/script.js
@@ -1,6 +1,10 @@
 var app = new Vue({
     el: '#app',
     data: {
-        colors: ['Red', 'Green', 'Blue',]
+        user: {
+            firstName: 'Taro',
+            lastName: 'Yamada',
+            age: 28,
+        },
     }
 })
