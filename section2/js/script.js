bbc86b13a833eab8da009ea4944934175741bf4d
Tatsuro <contacts@tatsuro.dev>
2019-12-26 01:32:01 +0900

    様々な形式のreactive dataを定義 18

--- section2/js/script.js
+++ section2/js/script.js
@@ -3,5 +3,12 @@ var app = new Vue({
     el: '#app',
     data: {
         message: 'Hello Vue.js!',
+        count: 10,
+        user: {
+            lastName: 'Yamada',
+            firstName: 'Taro',
+            prefecture: 'Tokyo',
+        },
+        colors: ['Red', 'Green', 'Blue',]
     }
 })
