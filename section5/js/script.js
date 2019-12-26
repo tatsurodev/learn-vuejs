a38d74eabe195a4a6a71cc912fc0d2893bc3615a
Tatsuro <contacts@tatsuro.dev>
2019-12-26 19:20:29 +0900

    v-bindの省略記法 76

--- section5/js/script.js
+++ section5/js/script.js
@@ -1,10 +1,6 @@
-Vue.filter('readMore', function (text, length, suffix) {
-    return text.substring(0, length) + suffix
-})
-
 var app = new Vue({
     el: '#app',
     data: {
-        text: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.'
+        url: 'https://www.google.co.jp/',
     },
 })
