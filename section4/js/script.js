f01cdf58bef88e2736a84dbcd174d2cce8825166
Tatsuro <contacts@tatsuro.dev>
2019-12-26 10:59:31 +0900

    axiosでapiからbpiを取得し、dataに格納 54

--- section4/js/script.js
+++ section4/js/script.js
@@ -4,6 +4,10 @@ var app = new Vue({
         bpi: null,
     },
     mounted: function () {
-
+        axios.get('https://api.coindesk.com/v1/bpi/currentprice.json').then(function (response) {
+            // console.log(response.data.bpi)
+            // console.log(response.data.bpi.USD.rate_float)
+            this.bpi = response.data.bpi
+        }.bind(this))
     }
 })
