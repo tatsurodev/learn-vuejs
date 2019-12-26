d3ae7c88af1e52f26821023d044cb684857aed6c
Tatsuro <contacts@tatsuro.dev>
2019-12-26 18:07:19 +0900

    global filterの作成と使用 73

--- section5/js/script.js
+++ section5/js/script.js
@@ -1,11 +1,16 @@
+// 他のcomponent等でも使用するfilterの登録にglobal filterは便利
+Vue.filter('numberFormat', function (value) {
+    return value.toLocaleString()
+})
+
 var app = new Vue({
     el: '#app',
     data: {
         price: 29800,
     },
-    filters: {
-        numberFormat: function (value) {
-            return value.toLocaleString()
-        }
-    }
+    // filters: {
+    //     numberFormat: function (value) {
+    //         return value.toLocaleString()
+    //     }
+    // },
 })
