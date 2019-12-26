368f64b03019b793fe468fad548ecd1a8c110363
Tatsuro <contacts@tatsuro.dev>
2019-12-26 16:37:34 +0900

    通信error handling追加 61

--- section4/js/script.js
+++ section4/js/script.js
@@ -2,6 +2,7 @@ var app = new Vue({
     el: '#app',
     data: {
         bpi: null,
+        hasError: false,
     },
     mounted: function () {
         axios.get('https://api.coindesk.com/v1/bpi/currentprice.json').then(function (response) {
@@ -10,7 +11,8 @@ var app = new Vue({
             this.bpi = response.data.bpi
         }.bind(this)).catch(function (error) {
             console.log(error)
-        })
+            this.hasError = true
+        }.bind(this))
     },
     filters: {
         currencyDecimal(value) {
