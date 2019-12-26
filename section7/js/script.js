018ed68341a44f82c29a2736daa1de7feee38b9d
Tatsuro <contacts@tatsuro.dev>
2019-12-26 22:33:46 +0900

    computedとwatched propertyの違い 85
    一般的にcomputed propertyの方が簡単に書ける

--- section7/js/script.js
+++ section7/js/script.js
@@ -1,26 +1,33 @@
+/*
+// watched property
 var app = new Vue({
     el: '#app',
     data: {
-        km: 0,
-        m: 0,
-        mm: 0,
+        firstName: '',
+        lastName: '',
+        fullName: '',
     },
     watch: {
-        km: function (value) {
-            // console.log(value)
-            this.km = value
-            this.m = value * 1000
-            this.mm = value * 1000000
+        firstName: function (value) {
+            this.fullName = value + ' ' + this.lastName
         },
-        m: function (value) {
-            this.km = value / 1000
-            this.m = value
-            this.mm = value * 1000
-        },
-        mm: function (value) {
-            this.km = value / 1000000
-            this.m = value / 1000
-            this.mm = value
+        lastName: function (value) {
+            this.fullName = this.firstName + ' ' + value
+        }
+    },
+})
+*/
+
+// computed property
+var app = new Vue({
+    el: '#app',
+    data: {
+        firstName: '',
+        lastName: '',
+    },
+    computed: {
+        fullName: function () {
+            return this.firstName + ' ' + this.lastName
         },
     },
 })
