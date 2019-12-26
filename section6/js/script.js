796caeeaf7f257e0daa2317197b256cf773ae64e
Tatsuro <contacts@tatsuro.dev>
2019-12-26 21:07:33 +0900

    computed propertyとmethodの違い 79

--- section6/js/script.js
+++ section6/js/script.js
@@ -3,8 +3,17 @@ var app = new Vue({
     data: {
         message: 'Hello Vue.js!',
     },
+    // 依存関係が変化しない限りcached
+    // getter, setter両方可
     computed: {
         reversedMessage: function () {
+            return this.message.split('').reverse().join('')
+        },
+    },
+    // not cached
+    // getterのみ
+    methods: {
+        reversedMessageMethod: function () {
             return this.message.split('').reverse().join('')
         },
     },
