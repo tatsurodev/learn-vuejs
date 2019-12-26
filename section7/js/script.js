714ed6a6a7b4bbb5f53359e6009f77c5db371da7
Tatsuro <contacts@tatsuro.dev>
2019-12-27 00:01:34 +0900

    配列、objectをwatchする時は、newとoldの値が参照渡しで同じになるので注意 88

--- section7/js/script.js
+++ section7/js/script.js
@@ -9,8 +9,17 @@ var app = new Vue({
     },
     watch: {
         colors: {
+            // 配列、objectのwatchにはhandlerとdeep propertyの設定が必要
             handler: function (newValue, oldValue) {
                 console.log('Update!')
+                console.log(
+                    // 配列、objectに対してwatcherを作成した時は、newとoldが値渡しでなく参照渡しとなるため、同じ参照を返すこととなりnewとoldが同じ値となるので注意が必要
+                    'new: %s, old: %s',
+                    // JSON.stringifyでjs objectをjsonに変換
+                    // JSON.parseは、その逆でjsonをjs objectに変換
+                    JSON.stringify(newValue, null, '\t'),
+                    JSON.stringify(oldValue, null, '\t')
+                )
             },
             deep: true,
         },
