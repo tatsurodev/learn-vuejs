c2d6c2486898ffb51292714999f7307d86b3c7c2
Tatsuro <contacts@tatsuro.dev>
2019-12-26 19:16:43 +0900

    filterに引数追加 75

--- section5/js/script.js
+++ section5/js/script.js
@@ -1,14 +1,10 @@
-Vue.filter('toUSD', function (jpy) {
-    return jpy / 100
-})
-
-Vue.filter('numberFormat', function (value) {
-    return value.toLocaleString()
+Vue.filter('readMore', function (text, length, suffix) {
+    return text.substring(0, length) + suffix
 })
 
 var app = new Vue({
     el: '#app',
     data: {
-        jpyPrice: 298000000,
+        text: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.'
     },
 })
