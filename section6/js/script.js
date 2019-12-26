38746ed4765bc1c78e65046e38962042a5ad6f74
Tatsuro <contacts@tatsuro.dev>
2019-12-26 21:49:04 +0900

    computed propertyとmethodのcache 81

--- section6/js/script.js
+++ section6/js/script.js
@@ -1,16 +1,13 @@
 var app = new Vue({
     el: '#app',
-    data: {
-        basePrice: 100,
-    },
     computed: {
-        taxIncludedPrice: {
-            get: function () {
-                return parseInt(this.basePrice * 1.08)
-            },
-            set: function (taxIncludedPrice) {
-                this.basePrice = Math.ceil(taxIncludedPrice / 1.08)
-            },
+        computedNumber: function () {
+            return Math.random()
+        },
+    },
+    methods: {
+        methodsNumber: function () {
+            return Math.random()
         },
     },
 })
