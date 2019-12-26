b1f8dfb619bc4b466207e07c592fa9cd4a51949a
Tatsuro <contacts@tatsuro.dev>
2019-12-27 03:11:38 +0900

    axiosでqiitaのapiからdata取得 95

--- section8/js/script.js
+++ section8/js/script.js
@@ -8,10 +8,36 @@ var app = new Vue({
     watch: {
 
     },
-    created: {
-
+    created: function () {
+        this.keyword = 'Javascript'
+        this.getAnswer()
     },
     methods: {
-
+        getAnswer: function () {
+            if (this.keyword === '') {
+                this.items = null
+                return
+            }
+            this.message = 'Loading...'
+            // axios内でthis.dataにaccessできるようthisを変数に格納
+            var vm = this
+            // axios用query paramsを用意
+            var params = {
+                page: 1,
+                per_page: 20,
+                query: this.keyword,
+            }
+            axios
+                .get('https://qiita.com/api/v2/items', { params })
+                .then(function (response) {
+                    console.log(response)
+                    vm.items = response.data
+                })
+                .catch(function (error) {
+                    vm.message = 'Error!' + error
+                }).finally(function () {
+                    vm.message = ''
+                })
+        },
     },
 })
