b6cca4b22e237fcd8de680a21d3e5f06d92445c9
Tatsuro <contacts@tatsuro.dev>
2019-12-26 11:03:48 +0900

    errorをcatch 56

--- section4/js/script.js
+++ section4/js/script.js
@@ -4,10 +4,12 @@ var app = new Vue({
         bpi: null,
     },
     mounted: function () {
-        axios.get('https://api.coindesk.com/v1/bpi/currentprice.json').then(function (response) {
+        axios.get('https://api.coindesk.com/v1/bpi/currentprice.jsonn').then(function (response) {
             // console.log(response.data.bpi)
             // console.log(response.data.bpi.USD.rate_float)
             this.bpi = response.data.bpi
-        }.bind(this))
+        }.bind(this)).catch(function (error) {
+            console.log(error)
+        })
     }
 })
