42a627987fd09137e75d69c54f3694e2baa5e773
Tatsuro <contacts@tatsuro.dev>
2019-12-27 20:41:30 +0900

    componentのlocal登録 140

--- section13/js/script.js
+++ section13/js/script.js
@@ -1,7 +1,11 @@
-Vue.component('hello-component', {
+var helloComponent = {
     template: '<p>Hello</p>',
-})
+}
 
 var app = new Vue({
     el: '#app',
+    components: {
+        // component名はケバブケースでないとダメ
+        'hello-component': helloComponent,
+    },
 })
