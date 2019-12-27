b6cfae833fb02efbf57d47ec3346225bd7b61b56
Tatsuro <contacts@tatsuro.dev>
2019-12-27 21:54:46 +0900

    componentにdataを持たせる 142

--- section13/js/script.js
+++ section13/js/script.js
@@ -1,11 +1,14 @@
-var helloComponent = {
-    template: '<p>Hello</p>',
-}
+Vue.component('button-counter', {
+    // component内のdataはobjectを返す関数である必要がある。それによりscopeがcomponent毎に限定され、各々のdataを保持できる。無名関数でなく、arrow関数だとthisがwindow objectに固定されてしまうので使わないこと
+    data: function () {
+        return {
+            count: 0,
+        }
+    },
+    // templateは単一の要素である必要がある
+    template: '<div><span>count: </span><button v-on:click="count++">{{ count }}</button></div>',
+})
 
 var app = new Vue({
     el: '#app',
-    components: {
-        // component名はケバブケースでないとダメ
-        'hello-component': helloComponent,
-    },
 })
