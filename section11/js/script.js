c863c1cec8ea036b8a9e0c53ca958f4742a0af88
Tatsuro <contacts@tatsuro.dev>
2019-12-27 17:28:49 +0900

    event modifier .once使用 120

--- section11/js/script.js
+++ section11/js/script.js
@@ -1,14 +1,11 @@
 var app = new Vue({
     el: '#app',
     data: {
-        counter: 0,
         message: '',
     },
     methods: {
-        // $eventはvueで指定されているものなので$eとかではダメ
-        clickHandler: function ($event, message) {
-            this.message = message
-            console.log($event)
+        clickHandler: function () {
+            this.message = new Date().toLocaleTimeString()
         },
     },
 })
